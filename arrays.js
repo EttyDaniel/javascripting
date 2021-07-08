0aadb04881d041f5002864f6cc0ed7276e11f4f1
Eti Daniel <danieleti@gmail.com>
2021-07-08 10:53:37 -0400

    initial commit with all my work

---
+++ arrays.js
@@ -0,0 +1,4 @@
+let pizzaToppings = [" 'tomato sauce'"," 'cheese'"," 'pepperoni' "];
+//for(let i=0;i<pizzaToppings.length;i++)
+//	console.log(pizzaToppings[i]);
+console.log("["+pizzaToppings[0]+","+pizzaToppings[1]+","+pizzaToppings[2]+"]");
